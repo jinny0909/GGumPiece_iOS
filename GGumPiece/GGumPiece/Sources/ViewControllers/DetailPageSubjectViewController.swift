a08a81a91640c789d0c31a1da9c1bf43723975c4
parkikbum <pibum@naver.com>
2021-05-23 06:48:05 +0900

    [ADD]상세뷰 및 슬라이싱 삭제 구현

--- GGumPiece/GGumPiece/Sources/ViewControllers/DetailPageSubjectViewController.swift
+++ GGumPiece/GGumPiece/Sources/ViewControllers/DetailPageSubjectViewController.swift
@@ -4,18 +4,33 @@
 //
 //  Created by 박익범 on 2021/05/23.
 //
-
 import UIKit
 
 class DetailPageSubjectViewController: UIViewController {
     @IBOutlet weak var subjectView: UIView!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var subjectLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    var date :String = ""
+    var subject : String = ""
+    var name : String = ""
     
     override func viewDidLoad() {
+        setRadius()
+        setLabel()
         super.viewDidLoad()
-
     }
+    func setLabel(){
+        dateLabel.text = date
+        subjectLabel.text = subject
+        nameLabel.text = name
+    }
+    
     func setRadius(){
         subjectView.clipsToBounds = true
-        subjectView.layer.cornerRadius = 20
+        subjectView.layer.cornerRadius = 30
+    }
+    @IBAction func backButtonClicke(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
     }
 }
