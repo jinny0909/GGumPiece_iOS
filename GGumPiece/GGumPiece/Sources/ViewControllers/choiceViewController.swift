5e23f4fb7cfd06322e68f59933c808c3963574e4
Kyoungjin Lim <klim30@jhu.edu>
2021-05-23 07:30:00 +0900

    no message

---
+++ GGumPiece/GGumPiece/Sources/ViewControllers/choiceViewController.swift
@@ -0,0 +1,54 @@
+//
+//  choiceViewController.swift
+//  GGumPiece
+//
+//  Created by kyoungjin on 2021/05/23.
+//
+
+
+import UIKit
+
+class choiceViewController: UIViewController {
+
+    static let identifier : String = "choiceViewController"
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBOutlet weak var joinButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    
+    @IBAction func joinClicked(_ sender: Any) {
+        guard let signupVC = self.storyboard?.instantiateViewController(identifier: "signupViewController")as? signupViewController else{return}
+        signupVC.modalPresentationStyle = .fullScreen
+        self.present(signupVC, animated: true)
+        
+    }
+    
+    
+    
+    @IBAction func loginClicked(_ sender: Any) {
+        guard let loginVC = self.storyboard?.instantiateViewController(identifier: "loginViewController")as? loginViewController else{return}
+        loginVC.modalPresentationStyle = .fullScreen
+        self.present(loginVC, animated: true)
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ GGumPiece/GGumPiece/Sources/ViewControllers/signupViewController.swift
@@ -0,0 +1,33 @@
+//
+//  signupViewController.swift
+//  GGumPiece
+//
+//  Created by kyoungjin on 2021/05/23.
+//
+
+import UIKit
+
+class signupViewController: UIViewController {
+
+    static let identifier : String = "signupViewController"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func joinClicked(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
